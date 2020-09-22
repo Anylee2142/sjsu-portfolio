d430f1704942857f4d5b5d179b1b28210dfdd76a
Anylee2142 <anylee2142@gmail.com>
2020-09-21 21:38:53 -0700

    Page Completed

---
+++ cmpe273-EntDev/lab1/README.md
@@ -0,0 +1,10 @@
+https://github.com/facebook/create-react-app
+1. npx create-react-app my-app
+2. cd my-app
+3. npm start
+
+React Bootstrap
+https://devlog.jwgo.kr/2019/01/17/how-to-use-bootstrap-in-react/
+1. npm install --save bootstrap
+2. import 'bootstrap/dist/css/bootstrap.css' at src/index.js
+
---
+++ cmpe273-EntDev/lab1/frontend/src/components/Header/Navbar.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+import { Link, NavLink } from 'react-router-dom';
+import cookie from 'react-cookies';
+import { Redirect } from 'react-router';
+
+//create the Navbar Component
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+    //handle logout to destroy the cookie
+    handleLogout = () => {
+        cookie.remove('cookie', { path: '/' })
+    }
+    render() {
+        //if Cookie is set render Logout Button
+        let navLogin = null;
+        if (cookie.load('cookie')) {
+            console.log("Able to read cookie");
+            navLogin = (
+                <ul class="nav navbar-nav navbar-right">
+                    <li><Link to="/" onClick={this.handleLogout}><span class="glyphicon glyphicon-user"></span>Logout</Link></li>
+                </ul>
+            );
+        } else {
+            //Else display login button
+            console.log("Not Able to read cookie");
+            navLogin = (
+                <ul class="nav navbar-nav navbar-right">
+                    <li><Link to="/login"><span class="glyphicon glyphicon-log-in"></span> Login</Link></li>
+                </ul>
+            )
+        }
+        let redirectVar = null;
+        if (cookie.load('cookie')) {
+            redirectVar = <Redirect to="/home" />
+        } else {
+            redirectVar = <Redirect to="/login" />
+        }
+        return (
+            <div>
+                {redirectVar}
+                <nav class="navbar navbar-default navbar-background">
+                    <div class="navbar-header">
+                        <a href="#" class="navbar-brand">Yelp</a>
+                    </div>
+
+                    <div class="container">
+
+                        <form class="navbar-form navbar-left">
+                            <div class="form-group">
+                                <input type="text" class="form-control"></input>
+                            </div>
+                            <button type="submit" class="btn btn-default"><span class="button-style">Search!</span></button>
+                        </form>
+
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Sign Up</a></li>
+                            <li><a href="#">Login</a></li>
+                        </ul>
+
+                    </div>
+
+                </nav>
+
+                <div class="header container">
+                    <div class="user-profile-picture">
+                        Add a photo !
+                    </div>
+                    <div class="brief-summary">
+                        <h1>Alan Lee</h1>
+                        <p>Downtown San Jose, San Jose, CA</p>
+                        <p>Friends, Reviews Photos</p>
+                    </div>
+                    <div class="modify-menu">
+                        <ul class="list-unstyled">
+                            <li><a href="#">Add Profile Photos</a></li>
+                            <li><a href="#">Update Your Profile</a></li>
+                            <li><a href="#">Find Friends</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="main-content container">
+                    <div class="profile-list">
+                        <h1>Alan's Profile</h1>
+                        <div class="list-group">
+                            <a href="#" class="list-group-item">Profile Overview</a>
+                            <a href="#" class="list-group-item">Friends</a>
+                            <a href="#" class="list-group-item">Reviews</a>
+                            <a href="#" class="list-group-item">Compliments</a>
+                            <a href="#" class="list-group-item">Tips</a>
+                            <a href="#" class="list-group-item">Bookmarks</a>
+                            <a href="#" class="list-group-item">Collections</a>
+                            <a href="#" class="list-group-item">Events</a>
+                        </div>
+                    </div>
+                    <div class="recent-activity">
+                        <h1>Notifications</h1>
+                        <p>Don't sleep !</p>
+                        <h1>Recent Activity</h1>
+                        <hr></hr>
+                    </div>
+                    <div class="about-user">
+                        <h1>About Alan Lee</h1>
+                        <ul class="list-unstyled">
+                            <li>
+                                <h2>Location</h2>
+                                <p>San Jose Downtown</p>
+                            </li>
+                            <li>
+                                <h2>Things I Love</h2>
+                                <p>Reading, Thinking</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Navbar;
